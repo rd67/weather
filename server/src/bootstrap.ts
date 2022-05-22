1d6afb255b6021dcc5da4b4d67e5591fb8fa61c3
Rohit Dalal <rohit@dev-story.com>
2022-05-22 18:43:06 +0530

    Cities Syncing from File Bootstraped

--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -1,16 +1,123 @@
 import { cyan } from "chalk";
+import { createReadStream } from "fs";
+import { join } from "path";
+import { parse as jsonParse } from "JSONStream";
+
+import { IPointType } from "@interfaces/common";
+
+import { convertToPoint, formatString } from "@helpers/common";
+
+import { logger, logInfo } from "@packages/logger";
 
 import models from "@models/index";
 
+interface ICityFileRow {
+  id: number;
+  name: string;
+  state: string;
+  country: string;
+  coord: {
+    lon: number;
+    lat: number;
+  };
+}
+
+interface ICityCreateRow {
+  id: number;
+  name: string;
+  state: string;
+  country: string;
+  point: IPointType;
+}
+
+/**
+ *
+ * @param filePath path of the cities file
+ * @returns {
+ * totalRows,
+ * processedRows
+ * }
+ */
+const bootstrapCities = async (filePath: string) => {
+  return new Promise((resolve, reject) => {
+    const MAX_BATCH_SIZE = 25000;
+
+    let totalRows = 0;
+    let processedRows = 0;
+
+    const stream = createReadStream(filePath, { encoding: "utf8" });
+    const parser = jsonParse("*");
+
+    stream.pipe(parser);
+
+    const flush = async (rows: ICityCreateRow[]) => {
+      parser.pause();
+
+      await models.City.bulkCreate(rows, {
+        logging: false,
+      });
+
+      processedRows += rows.length;
+
+      parser.resume();
+    };
+
+    // Current batch
+    let batch: ICityCreateRow[] = [];
+
+    logInfo({
+      msg: "Bootstraping Cities",
+      filePath,
+    });
+
+    parser
+      .on("data", (row: ICityFileRow) => {
+        batch.push({
+          id: parseInt(`${row.id}`),
+          name: formatString(row.name),
+          state: row.state,
+          country: row.country,
+          point: convertToPoint(row.coord.lat, row.coord.lon),
+        });
+
+        totalRows += 1;
+
+        if (batch.length >= MAX_BATCH_SIZE) {
+          flush(batch).catch(reject);
+          batch = [];
+        }
+      })
+      .on("end", () => {
+        flush(batch)
+          .catch(reject)
+          .then(() => {
+            resolve({
+              totalRows,
+              processedRows,
+            });
+          });
+      })
+      .on("error", reject);
+  }).catch((error) => {
+    logger.error(`Cities Bootstrap Error`, error);
+    throw error;
+  });
+};
+
 export const bootstrapApp = async () => {
   //  Cities
-  const cityCount = await models.City.count();
-  if (!cityCount) {
-    // models.Setting.create({
-    //   versioning: DefaultAppVersioning,
-    //   serviceFeePercents: DefaultServiceFeePercents,
-    //   defaultCancellationFee: DefaultCancellationFee,
-    // });
+  const totalRows = await models.City.count();
+  if (!totalRows) {
+    console.time("bootstrapCities");
+
+    const result = await bootstrapCities(join(__dirname, "city.list.json"));
+
+    console.timeEnd("bootstrapCities");
+
+    logInfo({
+      msg: "Cities Bootstraped",
+      result,
+    });
   }
 
   console.log(`${cyan("🌿 App Bootstrapped")} successfully.`);
--- server/src/helpers/common.ts
+++ server/src/helpers/common.ts
@@ -1,3 +1,4 @@
+import { IPointType } from "@interfaces/common";
 import { v4 as uuidv4 } from "uuid";
 
 export const uuidGenerate = () => {
@@ -16,4 +17,43 @@ export const pick = (object: Object, keys: string[]): object => {
 
 export const capitalizeFirstLetter = (value: string): string => {
   return value[0].toUpperCase() + value.slice(1);
+};
+
+export const convertToPoint = (
+  latitude: number,
+  longitude: number
+): IPointType => {
+  return {
+    type: "Point",
+    coordinates: [longitude, latitude], // GeoJson format: [lng, lat]
+    crs: { type: "name", properties: { name: "EPSG:4326" } },
+  };
+};
+
+export const formatString = (str: string) => {
+  //  Removing trailing, leading `
+  if (str.startsWith("`")) {
+    str = str.slice(1);
+  }
+  if (str.endsWith("`")) {
+    str = str.slice(0, -1);
+  }
+
+  //  Removing trailing, leading "
+  if (str.startsWith('"')) {
+    str = str.slice(1);
+  }
+  if (str.endsWith('"')) {
+    str = str.slice(0, -1);
+  }
+
+  //  Removing trailing, leading "
+  if (str.startsWith("'")) {
+    str = str.slice(1);
+  }
+  if (str.endsWith("'")) {
+    str = str.slice(0, -1);
+  }
+
+  return str.trim();
 };
